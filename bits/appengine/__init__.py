17c3c864ecfcd39277c1eb4ff783644a4ca4aa9f
Lukas Karlsson <karlsson@broadinstitute.org>
2019-10-11 03:01:49 +0000

    Added support for config settings and user info from headers and admin info from database.

--- bits/appengine/__init__.py
+++ bits/appengine/__init__.py
@@ -14,25 +14,38 @@ class AppEngine(object):
 
     def __init__(
         self,
-        database='firestore',
-        project=None,
+        config_collection='settings',
+        config_database='firestore',
+        config_project=None,
+        debug_user=None,
         user_collection='users',
-        debug_user=None
+        user_database='firestore',
+        user_project=None,
     ):
         """Initialize an Example class instance."""
-        self.database = database
+        self.config_collection = config_collection
+        self.config_database = config_database
+        self.config_project = config_project
         self.debug_user = debug_user
+        self.user_collection = user_collection
+        self.user_database = user_database
+        self.user_project = user_project
 
     def config(self):
         """Return a Config instance."""
         return Config(
             appengine=self,
-            database=self.database,
+            collection=self.config_collection,
+            database=self.config_database,
+            project=self.config_project,
         )
 
     def user(self):
         """Return a User instance."""
         return User(
             appengine=self,
-            database=self.database,
+            collection=self.user_collection,
+            database=self.user_database,
+            debug_user=self.debug_user,
+            project=self.user_project,
         )
--- bits/appengine/user.py
+++ bits/appengine/user.py
@@ -1,46 +1,92 @@
 # -*- coding: utf-8 -*-
 """App Engine User class file."""
 
-# from flask import request
-# from google.cloud import firestore
+import logging
+from flask import request
+from google.cloud import datastore
+from google.cloud import firestore
 
 
 class User(object):
-    """Users class."""
+    """Uses class."""
 
-    def __init__(self, debug_user=None):
+    def __init__(
+        self,
+        appengine=None,
+        collection='users',
+        database='firestore',
+        debug_user=None,
+        project=None
+    ):
         """Initialize a class instance."""
+        self.appengine = appengine
+        self.collection = collection
+        self.database = database
+        self.debug_user = debug_user
+        self.project = project
+
+        self.admin = False
         self.email = None
         self.id = None
 
+        # get the logged-in user
+        if self.debug_user:
+            # get the debug user
+            self.get_debug_user()
+        else:
+            # get the current logged-in user from headers
+            self.get_current_user()
+
+        # get stored user from the database
+        self.get_stored_user()
+
+        # set user as a dict
+        self.user = {'email': self.email, 'id': self.id}
+
+    def __getitem__(self, key):
+        """Return the value of the given attribute."""
+        return self.__dict__[key]
+
+    def __str__(self):
+        """Return the string representation."""
+        return 'User: {} [{}]'.format(self.email, self.id)
+
+    def get(self, key):
+        """Return the value of the given attribute."""
+        return self.__getitem__(key)
+
     def get_current_user(self):
         """Return the current user."""
+        # define headers
+        user_email_header = 'X-Goog-Authenticated-User-Email'
+        user_id_header = 'X-Goog-Authenticated-User-ID'
+
+        # get Google IAP user's email and id from headers
+        self.email = request.headers.get(user_email_header).replace('accounts.google.com:', '')
+        self.id = request.headers.get(user_id_header).replace('accounts.google.com:', '')
 
     def get_debug_user(self):
         """Return the debug user."""
+        self.email = self.debug_user.get('email')
+        self.id = self.debug_user.get('id')
 
+    def get_stored_datastore_user(self):
+        """Return the user info from datastore."""
+        client = datastore.Client(project=self.project)
+        return client.get(client.key(self.collection, self.id))
 
-    def email(self):
-        """Return the Google primary email of the logged-in user."""
-        return self.user().get('email')
+    def get_stored_firestore_user(self):
+        """Return the user info from firestore."""
+        db = firestore.Client(project=self.project)
+        user = db.collection(self.collection).document(self.id).get().to_dict()
+        if user.get('admin'):
+            self.admin = True
 
-    def user(self):
-        """Return the current user."""
-        # for debugging, return the debug user passed to the function
-        if self.debug_user:
-            return self.debug_user
-
-        # get user email and id from headers
-        google_email = request.headers.get('X-Goog-Authenticated-User-Email')
-        google_id = request.headers.get('X-Goog-Authenticated-User-ID')
-
-        # create the user dict
-        user_data = {
-            'email': google_email.replace('accounts.google.com:', ''),
-            'id': google_id.replace('accounts.google.com:', ''),
-        }
-        return user_data
-
-    def user_id(self):
-        """Return the Google ID of the logged-in user."""
-        return self.user().get('id')
+    def get_stored_user(self):
+        """Return the user info from the database."""
+        if self.database == 'firestore':
+            return self.get_stored_firestore_user()
+        elif self.database == 'database':
+            return self.get_stored_datastore_user()
+        else:
+            logging.error('Unsupported database: {}'.format(self.database))
