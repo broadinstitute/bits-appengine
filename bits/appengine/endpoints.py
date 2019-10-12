43125c38bf1d95cefdeaa5dd0e65c646f6336f8d
Lukas Karlsson <karlsson@broadinstitute.org>
2019-10-12 04:57:34 +0000

    Added basic methods to the endpoints client.

--- bits/appengine/endpoints.py
+++ bits/appengine/endpoints.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """Endpoints Class file."""
 
 import requests
@@ -19,7 +18,7 @@ class Endpoints(object):
             self,
             api_key=None,
             base_url='http://localhost:8080',
-            api='bitsdb',
+            api='api',
             version='v1',
             verbose=False,
         ):
@@ -30,10 +29,10 @@ class Endpoints(object):
             self.verbose = verbose
             self.version = version
 
-            self.api_url = '%s/_ah/api/%s/%s' % (self.base_url, self.api, self.version)
+            self.api_url = '{}/_ah/api/{}/{}'.format(self.base_url, self.api, self.version)
 
             # generate discovery url
-            self.discovery_url = '%s/_ah/api/discovery/v1/apis/%s/%s/rest' % (
+            self.discovery_url = '{}/_ah/api/discovery/v1/apis/{}/{}/rest'.format(
                 self.base_url,
                 self.api,
                 self.version
@@ -45,7 +44,7 @@ class Endpoints(object):
                 'my-user-agent/1.0'
             )
 
-            # create a bitsdb service connection
+            # create a service connection
             self.service = build(
                 self.api,
                 self.version,
@@ -80,7 +79,7 @@ class Endpoints(object):
             return jwt.encode(credentials.signer, payload)
 
         def get_id_token(self):
-            """Return an access token for connecting to BITSdb."""
+            """Return an access token for connecting to Endpoints APIs."""
             headers = {"Content-Type": "application/x-www-form-urlencoded"}
             params = {
                 'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
@@ -89,3 +88,163 @@ class Endpoints(object):
             url = 'https://www.googleapis.com/oauth2/v4/token'
             # send the JWT to Google Token endpoints to request Google ID token
             return requests.post(url, data=params, headers=headers).json().get('id_token')
+
+        #
+        # Basic Methods (GET, POST, etc.)
+        #
+        def delete(self, path, credentials=None):
+            """Return the response from a DELETE request."""
+            url = '{}/{}'.format(self.api_url, path)
+
+            headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+
+            if credentials:
+                credentials.apply(headers)
+
+            params = {
+                'key': self.api_key
+            }
+
+            response = requests.delete(
+                url,
+                headers=headers,
+                params=params,
+                verify=False,
+            )
+            response.raise_for_status()
+            return response.json()
+
+        def get(self, path, credentials=None, limit=None, pageToken=None):
+            """Return the response from a GET request."""
+            url = '{}/{}'.format(self.api_url, path)
+
+            headers = {
+                'Accept': 'application/json',
+            }
+
+            if credentials:
+                credentials.apply(headers)
+
+            params = {
+                'key': self.api_key
+            }
+
+            if limit:
+                params['limit'] = limit
+            if pageToken:
+                params['pageToken'] = pageToken
+
+            # print(json.dumps(headers, indent=2, sort_keys=True)
+
+            response = requests.get(
+                url,
+                params=params,
+                headers=headers,
+                verify=False
+            )
+            response.raise_for_status()
+            return response.json()
+
+        def post(self, path, data, credentials=None):
+            """Return the response from a POST request."""
+            url = '{}/{}'.format(self.api_url, path)
+
+            headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+
+            if credentials:
+                credentials.apply(headers)
+
+            params = {
+                'key': self.api_key
+            }
+
+            response = requests.post(
+                url,
+                headers=headers,
+                params=params,
+                json=data,
+                verify=False,
+            )
+            response.raise_for_status()
+            return response.json()
+
+        def put(self, path, data, credentials=None):
+            """Return the response from a PUT request."""
+            url = '{}/{}'.format(self.api_url, path)
+
+            headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+
+            if credentials:
+                credentials.apply(headers)
+
+            params = {
+                'key': self.api_key
+            }
+
+            response = requests.put(
+                url,
+                headers=headers,
+                params=params,
+                json=data,
+                verify=False,
+            )
+
+            response.raise_for_status()
+            return response.json()
+
+        def get_list(self, path, limit=None, credentials=None):
+            """Return a list of all items from a request."""
+            response = self.get(path, limit=limit, credentials=credentials)
+            if not response:
+                return []
+            items = response.get('items', [])
+            pageToken = response.get('nextPageToken')
+            while pageToken:
+                response = self.get(path, limit=limit, pageToken=pageToken, credentials=credentials)
+                items += response.get('items', [])
+                pageToken = response.get('nextPageToken')
+            return items
+
+        def get_paged_list(self, request, params={}):
+            """Return a list of all items from a request."""
+            response = request.list(**params).execute()
+            if not response:
+                return []
+            items = response.get('items', [])
+            pageToken = response.get('nextPageToken')
+            while pageToken:
+                params['pageToken'] = pageToken
+                response = request.list(**params).execute()
+                items += response.get('items', [])
+                pageToken = response.get('nextPageToken')
+            return items
+
+        def get_dict(self, path, credentials=None, limit=None, key='id'):
+            """Return a dict of items from a resquest."""
+            items = self.get_list(path, limit, credentials)
+            data = {}
+            for i in items:
+                k = i.get(key)
+                data[k] = i
+            return data
+
+        def to_dict(self, items, key='id'):
+            """Return a dict of items."""
+            data = {}
+            duplicates = []
+            for i in items:
+                k = i.get(key)
+                if k not in data:
+                    data[k] = i
+                else:
+                    duplicates.append(i)
+            return data, duplicates
